c02dea5c6ec19f1b61c8232d56ca521b57b52346
Slav Tsekov <slav.tsekov@gmail.com>
2018-05-13 19:35:46 +0300

    Init project

---
+++ src/routers/PublicRoute.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Route, Redirect } from "react-router-dom";
+
+const PublicRoute = ({ isAuthenticated, component: Component, ...rest }) => {
+    const authConditionComponent = (props) => isAuthenticated ? <Redirect to="/dashboard" /> : <Component {...props} />;
+    return <Route {...rest} component={authConditionComponent} />
+};
+
+const mapStateToProps = (state) => ({
+    isAuthenticated: !!state.auth.uid
+});
+
+const PublicRouteWithStore = connect(mapStateToProps)(PublicRoute);
+
+export { PublicRoute, PublicRouteWithStore as default };
